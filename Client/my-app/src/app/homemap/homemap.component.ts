f8714344ece8266b621e9ace8eeed3aaa88fc943
João Vaz <jonh_vz_619@hotmail.com>
2019-06-16 17:11:39 +0100

    -Map já mostra a localizaçao das OBU's automaticamente

--- Client/my-app/src/app/homemap/homemap.component.ts
+++ Client/my-app/src/app/homemap/homemap.component.ts
@@ -6,6 +6,7 @@ import { User } from '../Model/User';
 import { LocalStorageService } from '../_services/localStorage.service';
 import { Router } from '@angular/router';
 import { OBUStatus } from '../Model/OBUStatus';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-homemap',
@@ -21,10 +22,11 @@ export class HomemapComponent implements OnInit {
   ) { }
 
   private obus: OBU[];
-  private positions: OBUStatus[];
+  private positions: OBUStatus[] = new Array();
   private user: User;
   private radioValue: number;
-
+  private map : L.Map //Mapa a ser usado
+  private markerArray = new Array; //Array de Markers
 
   ngOnInit() {
     this.user = this._localStorage.getCurrentUserDetails()
@@ -35,9 +37,12 @@ export class HomemapComponent implements OnInit {
         this.orderById()
         this.obus.forEach(obu => { //Em cada OBU, fazer o pedido das localizaçoes
           this._obuService.getPositionFromOBU(obu.id).subscribe( obuStatus =>{
-            //alert(JSON.stringify(obuStatus))
             obuStatus.obuId = obu.id
+            //console.log("obuStatus0:" + JSON.stringify(obuStatus))
+            //console.log("position:0" + JSON.stringify(this.positions))
             this.positions.push(obuStatus)
+            //console.log("obuStatus:" + JSON.stringify(obuStatus))
+            //console.log("position:" +  JSON.stringify(this.positions))
           })
         });
         
@@ -66,8 +71,9 @@ export class HomemapComponent implements OnInit {
   };
 
   
-  onMapReady(map: Map) {
-    map.setView([38.7573838, -9.1153841], 74)
+  onMapReady(map: L.Map) {
+    this.map = map;
+    this.map.setView([38.7573838, -9.1153841], 74)
     
     //var myMarker = marker([38.7573838, -9.1153841]).addTo(map);
     this.myMarker.bindPopup("<b>Hello world!</b><br>I am a popup, and this is ISEL!").openPopup();
@@ -78,8 +84,22 @@ export class HomemapComponent implements OnInit {
   }
 
   showLastPlace(){
-    this.options.center = latLng([55.7573838, -5.1153841])
     alert("showLastPlace");
+    //this.map.setView([38.7573838, -9.1153841], 15);
+    var latitude,longitude, coordenadas, newMarker
+    this.obus.forEach(obu=>{
+      coordenadas = this.positions.filter(pos => obu.id == pos.obuId )[0].coordinates
+      if(coordenadas.length != 0){//Caso noa haja info das coords de certa obu, nada é feito
+        latitude = coordenadas[coordenadas.length-1].latitude
+        longitude = coordenadas[coordenadas.length-1].longitude
+        newMarker = this.addNewMarkerToMap(latitude, longitude, obu.obuName)//cria o novo marker
+        this.markerArray.push(newMarker)//Adiciona o marker ao array de markers
+      }
+      
+    })
+    
+    var group = L.featureGroup(this.markerArray).addTo(this.map);
+    this.map.fitBounds(group.getBounds());
   }
 
   showLast24h(){
@@ -93,4 +113,18 @@ export class HomemapComponent implements OnInit {
   showLast72h(){
     alert("showLast72h");
   }
+
+  addNewMarkerToMap(latitude: number, longitude: number, msg: String){
+    return new L.Marker([latitude, longitude], 
+      {
+        icon: icon({
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: 'leaflet/marker-icon.png',
+          shadowUrl: 'leaflet/marker-shadow.png'
+        })
+      }).
+    bindPopup("<b>" + msg.toString() + "</b>").openPopup().addTo(this.map)
+  }
+  
 }
